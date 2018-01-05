d7b2f805f001d3881ee4669ccc93333b5002f64b
Sherry El Garhy <sherryptk@gmail.com>
2018-01-05 22:37:08 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item_requested = req.path.split("/items/").last
+
+      if item = @@items.find{|i| i.name == item_requested}
+      resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+      
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
